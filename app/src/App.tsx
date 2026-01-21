a214ca4393cc588f19ef3e1aa6aad5abfa4e80df
Micheal Wells <micheal.w.wells@gmail.com>
2026-01-21 14:18:06 -0800

    can get unstructured notes from file ready for llm call

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,31 @@
+import { readString } from 'react-papaparse'
 import './App.css'
 import Dropzone from 'react-dropzone'
+import { useEffect, useState } from 'react'
 
 
 function App() {
+
+  const [unstructuredFieldName, setUnstructuredFieldName] = useState('')
+  const [unstructuredFieldValues, setUnstructuredFieldValues] = useState([])
+  const[listOfDataFromUnstructuredField, setListOfDataFromUnstructuredField] = useState([])
+  const[asCSVData, setAsCSVData] = useState<any>([])
+
+  useEffect(() => {
+    console.log(unstructuredFieldName)
+    if (asCSVData.data) {
+      const listOfDataFromUnstructuredField = asCSVData.data.map((row: any) => row[unstructuredFieldName])
+      setListOfDataFromUnstructuredField(listOfDataFromUnstructuredField)
+    }
+  }, [unstructuredFieldName])
+
+  useEffect(() => {
+    console.dir(asCSVData)
+  }, [asCSVData])
+
+  useEffect(() => {
+    console.log(listOfDataFromUnstructuredField)
+  }, [listOfDataFromUnstructuredField])
 
   return (
     <>
@@ -16,10 +39,18 @@ function App() {
             reader.onerror = () => console.log('file reading has failed')
             reader.onload = () => {
             // Do whatever you want with the file contents
-              const binaryStr = reader.result
-              console.log(binaryStr)
+              const textResult : any = reader.result
+              const asCSVData = readString(textResult, {header: true, complete: (results) => {
+                console.log(results)
+                setAsCSVData(results as any)
+              }})
+              console.dir(asCSVData)
+              // put all headers into the dropdown
+              const headers = (asCSVData as any).meta.fields
+              setUnstructuredFieldValues(headers)
+              console.log(headers)
             }
-            reader.readAsArrayBuffer(file)
+            reader.readAsText(file)
           })
         }}>
           {({getRootProps, getInputProps}) => (
@@ -32,6 +63,17 @@ function App() {
           )}
         </Dropzone>
       </div>
+      <div id="input_unstructured_field_name">
+        <label htmlFor="input_unstructured_field_name">Unstructured Field Name</label>
+        <select value={unstructuredFieldName} id="input_unstructured_field_name"
+        onChange={(e) => setUnstructuredFieldName(e.target.value)}
+        >
+          {unstructuredFieldValues.map((value) => (
+            <option value={value}>{value}</option>
+          ))}
+        </select>
+      </div>
+
     </>
   )
 }
